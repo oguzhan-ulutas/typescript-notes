4443d10ee2b03d580b27abdae8c2c396c43f67c4
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-01-25 00:05:44 +0300

    Add variablesMe

---
+++ 01-intro/variableMe.js
@@ -0,0 +1,6 @@
+var greetings = 'Hello Jane';
+console.log(greetings);
+// Number
+var userId = 23456;
+// Boolean
+var isLoggedIn = false;
---
+++ 01-intro/variableMe.ts
@@ -0,0 +1,10 @@
+const greetings: string = 'Hello Jane';
+
+console.log(greetings);
+
+// Number
+
+const userId: number = 23456;
+
+// Boolean
+const isLoggedIn: boolean = false;
