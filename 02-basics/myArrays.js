01db85b80a97bbdc6824d9c54024d1adb6e51bfd
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-01-25 02:08:34 +0300

    Add myArrays

---
+++ 02-basics/myArrays.js
@@ -0,0 +1,7 @@
+const superHeros = [];
+// const heroPower: number[] = [];
+const heroPower = [];
+const allUsers = [];
+superHeros.push('Spiderman');
+heroPower.push(2);
+allUsers.push({ name: 'Jane', isActive: false });
---
+++ 02-basics/myArrays.ts
@@ -0,0 +1,14 @@
+const superHeros: string[] = [];
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+superHeros.push('Spiderman');
+heroPower.push(2);
+allUsers.push({ name: 'Jane', isActive: false });
